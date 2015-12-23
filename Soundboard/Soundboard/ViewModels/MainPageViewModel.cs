62870509c1cd0ed8fb70e44d8b10ce0a68099307
Niels Abels <n.abels@coolblue.nl>
2015-12-23 15:14:17 +0100

    Wired up the app to Prism.

---
+++ Soundboard/Soundboard/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Prism.Windows.Navigation;
+
+namespace Soundboard.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        public void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigatingFrom(NavigatingFromEventArgs e, Dictionary<string, object> viewModelState, bool suspending)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

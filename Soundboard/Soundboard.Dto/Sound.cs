4ab2381e0541d2b43b6584b8051b147bf1d93ab0
Niels Abels <n.abels@coolblue.nl>
2015-12-28 16:04:55 +0100

    Added Services and Dto project.

---
+++ Soundboard/Soundboard.Dto/Sound.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace Soundboard.Dto
+{
+    public class Sound : BindableBase
+    {
+        private string _displayName;
+        public string DisplayName
+        {
+            get { return _displayName; }
+            set { SetProperty(ref _displayName, value); }
+        }
+    }
+}
---
+++ Soundboard/Soundboard.Services/SoundService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Soundboard.Dto;
+
+namespace Soundboard.Services
+{
+    public interface ISoundService
+    {
+        List<Sound> GetAllSounds();
+    }
+
+    public class SoundService : ISoundService
+    {
+        public List<Sound> GetAllSounds()
+        {
+            var sounds = new List<Sound>()
+            {
+                new Sound() {DisplayName = "He meisje"},
+                new Sound() {DisplayName = "Ik heb een mop"},
+            };
+
+            return sounds;
+        }
+    }
+}

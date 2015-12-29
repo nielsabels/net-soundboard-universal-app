d10fe7611851c3ca766737585a16432c92669c5c
Niels Abels <n.abels@coolblue.nl>
2015-12-29 15:22:45 +0100

    Added DTO project to organize Dto objects for Azure API App.

---
+++ Soundboard/Soundboard.AzureApiApp.Dto/Sound.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soundboard.AzureApiApp.Dto
+{
+    public class Sound
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+}

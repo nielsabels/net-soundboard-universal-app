8ade5870706aa7fc1220bc6426ec1c88239aabf4
Niels Abels <n.abels@coolblue.nl>
2015-12-30 10:28:27 +0100

    Selected sound now delegates to command on viewmodel.

--- Soundboard/Soundboard/ViewModels/MainPageViewModel.cs
+++ Soundboard/Soundboard/ViewModels/MainPageViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.System.Power.Diagnostics;
 using Prism.Mvvm;
 using Prism.Windows.Navigation;
 using Soundboard.Dto;
@@ -13,12 +15,27 @@ namespace Soundboard.ViewModels
 {
     public class MainPageViewModel : BindableBase
     {
-        public ObservableCollection<Sound> Sounds { get; private set; } 
+        public ObservableCollection<Sound> Sounds { get; private set; }
+
+        private Sound _selectedSound;
+        public Sound SelectedSound
+        {
+            get { return _selectedSound; }
+            set
+            {
+                SetProperty(ref _selectedSound, value);
+                PlaySound(SelectedSound);
+            }
+        }
 
         public MainPageViewModel(ISoundService soundService)
         {
             Sounds = new ObservableCollection<Sound>(soundService.GetAllSounds());
         }
 
+        public void PlaySound(Sound sound)
+        {
+           
+        }
     }
 }

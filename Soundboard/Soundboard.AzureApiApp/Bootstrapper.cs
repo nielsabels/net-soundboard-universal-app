e4989de0eae432da46a41a03afeedee42922bd21
Niels Abels <n.abels@coolblue.nl>
2016-01-05 15:00:38 +0100

    Setup of bootstrapper complete.

---
+++ Soundboard/Soundboard.AzureApiApp/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Soundboard.AzureApiApp
+{
+    public class Bootstrapper
+    {
+        public static void Configure()
+        {
+            
+        }
+    }
+}
